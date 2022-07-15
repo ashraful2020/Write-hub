43bbbf247cf45c2662e0df5e368f82a222bb1363
Mohammed-Ashraful <mr.ashraful2020@gmail.com>
2022-07-15 18:42:53 +0600

    Added fancy navigation

---
+++ src/Pages/Shared/Navigation/NavLink.js
@@ -0,0 +1,101 @@
+import React, { useState } from "react";
+
+const NavLink = () => {
+  const links = [
+    { name: "Home", link: "Home", subMenu: false },
+    {
+      name: "Category",
+      link: "Category",
+      subMenu: true,
+      subLink: [
+        { name: "Life Style", link: "" },
+        { name: "Health", link: "" },
+        { name: "Family", link: "" },
+        { name: "Management", link: "" },
+        { name: "Travel", link: "" },
+        { name: "Work", link: "" },
+      ],
+    },
+    {
+      name: "Blog",
+      link: "blog",
+      subMenu: true,
+      subLink: [
+        { name: "Video Post", link: "Video Post" },
+        { name: "Audio Post", link: "Audio Post" },
+        { name: "Gallery Post", link: "Gallery Post" },
+        { name: "Standard Post", link: "Standard Post" },
+      ],
+    },
+    { name: "Styles", link: "Styles", subMenu: false },
+    { name: "About", link: "About", subMenu: false },
+    { name: "Contact", link: "Contact", subMenu: false },
+  ];
+
+  const [heading, setHeading] = useState(""); 
+
+  return (
+    <>
+      {links.map((link) => {
+        return (
+          <div className="group px-1 text-left md:cursor-pointer">
+            <h1
+              className="container flex justify-between text-left font-bold text-gray-400 hover:text-white"
+              onClick={() =>
+                heading !== link.name ? setHeading(link.name) : setHeading("")
+              }
+            >
+              {link.name}
+
+              {link.subMenu && (
+                <span className=" inline text-xl md:ml-2 md:block group-hover:md:-mt-2 group-hover:md:rotate-180">
+                  <ion-icon
+                    name={`${
+                      heading === link.name ? "chevron-up" : "chevron-down"
+                    }`}
+                  ></ion-icon>
+                </span>
+              )}
+            </h1>
+            {link.subMenu && (
+              <div>
+                <div className="top-15 absolute hidden hover:md:block group-hover:md:block">
+                  <div className="z-0 py-3">
+                    <div className="absolute left-3 mt-1 h-4 w-4 rotate-45 bg-black "></div>
+                  </div>
+                  <div className=" z-50  bg-black  px-6 py-4 ">
+                    {link.subLink.map((slink) => {
+                      return (
+                        <div>
+                          <h1 className="pt-2 font-normal text-gray-400 hover:font-semibold hover:text-white">
+                            {slink.name}
+                          </h1>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
+              </div>
+            )}
+
+            {/* mobile sub menu */}
+
+            {link.subMenu && (
+              <div
+                className={`${heading === link.name ? "md:hidden" : "hidden"}`}
+              >
+                {link.subLink.map((slink) => (
+                  <p className="pl-8 text-sm font-normal text-gray-400 hover:font-medium hover:text-white">
+                    {slink.name}
+                  </p>
+                ))}
+              </div>
+            )}
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+export default NavLink;
